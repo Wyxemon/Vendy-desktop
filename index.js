c52412ad8416fc3cfd525941d2df47fb858c5de0
Wyxemon <viscarretinigo@gmail.com>
2026-05-03 13:33:08 +0200

    Error solucionado

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-import { app, BrowserWindow, ipcMain, shell } from 'electron'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import { app, BrowserWindow, ipcMain, shell } from 'electron';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import { spawn } from "child_process";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-let server;
+const DEV_URL = "http://localhost:49173";
 
-function startServer() {
-    server = spawn('npx.cmd', ['serve', 'dist', '-l', '49173'], {
+function server() {
+    const dev = spawn('npm.cmd', ['run', 'dev'], {
         shell: true,
-        stdio: 'ignore',
+        stdio: 'inherit',
         windowsHide: true
     });
 }
 
 function createWindow() {
     const win = new BrowserWindow({
-        height: 600,
         width: 900,
+        height: 600,
         frame: false,
         icon: join(__dirname, 'icon.ico'),
         webPreferences: {
@@ -27,19 +27,20 @@ function createWindow() {
             contextIsolation: true,
             nodeIntegration: false
         }
-    })
+    });
 
-    win.loadURL("http://127.0.0.1:49173");
+    win.loadURL(DEV_URL);
 }
 
 app.whenReady().then(() => {
-    startServer();
     createWindow();
+    server();
 });
+
 ipcMain.on('open-url', (e, url) => {
-    shell.openExternal(url)
-})
+    shell.openExternal(url);
+});
 
 ipcMain.on('close-app', () => {
-    app.quit()
-})
+    app.quit();
+});
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  base: './'
-}
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    port: 49173,
+    strictPort: true
+  }
+});
